7cef909cbd1f0136fe90706d4e9edc8890b46915
hosseinbahmanpour <xbax@dhcp-asv-114.helsinki.fi>
2016-01-05 13:09:42 +0200

    pelilooppi fps lisätty

--- Avaruustaistelu/src/main/java/peli/avaruustaistelu/Main.java
+++ Avaruustaistelu/src/main/java/peli/avaruustaistelu/Main.java
@@ -1,20 +1,26 @@
 package peli.avaruustaistelu;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
 import peli.avaruustaistelu.kayttoliittyma.Kayttoliittyma;
 import peli.avaruustaistelu.logiikka.Laaseri;
 
 /**
- * Pelin main luokkassa on pelin gamelooppi
+ * Pelin main luokkassa on pelin gamelooppi ja törmäysten laskemisen metodi.
+ *
  * @author Hossein Bahmanpour
  */
-
 public class Main {
 
     public static void main(String[] args) {
         Avaruustaistelu a = new Avaruustaistelu();
         Kayttoliittyma k = new Kayttoliittyma(a);
         SwingUtilities.invokeLater(k);
+
+        long aloitusAika = System.currentTimeMillis();
+        int FPS = 25;
+        long haluttuAika = 1000 / FPS;
 
         while (true) {
 
@@ -30,8 +36,16 @@ public class Main {
                 }
             }
             k.getPaivitettava().paivita();
-            
-            System.out.println(a.getLaaserit());
+
+            long aikaNyt = System.currentTimeMillis();
+            long kulunutAika = aikaNyt - aloitusAika;
+
+            try {
+                Thread.sleep(haluttuAika - kulunutAika);
+            } catch (InterruptedException ex) {
+            }
+
+            aloitusAika = System.currentTimeMillis();
 
         }
     }
--- Avaruustaistelu/src/main/java/peli/avaruustaistelu/kayttoliittyma/NappaimistonKuuntelija.java
+++ Avaruustaistelu/src/main/java/peli/avaruustaistelu/kayttoliittyma/NappaimistonKuuntelija.java
@@ -7,35 +7,35 @@ import peli.avaruustaistelu.logiikka.Laaseri;
 import peli.avaruustaistelu.logiikka.Pelaaja;
 
 /**
- * Tässä luokassa kuunnellaan näppäimistöä ja muutetaan 
- * pelaajien alusten boolean arvoja sekä ammutaan.
+ * Tässä luokassa kuunnellaan näppäimistöä ja muutetaan pelaajien alusten
+ * boolean arvoja sekä ammutaan.
+ *
  * @author Hossein Bahmanpour
  */
-
 public class NappaimistonKuuntelija implements KeyListener {
 
     private Pelaaja p1;
     private Pelaaja p2;
     private ArrayList<Laaseri> laaserit;
-    
+
     /**
      * konstruktori.
+     *
      * @param p1 pelin eka pelaaja.
      * @param p2 pelin toka pelaaja.
      * @param laaserit arraylista pelissä olevista lasereista.
      */
-
     public NappaimistonKuuntelija(Pelaaja p1, Pelaaja p2, ArrayList<Laaseri> laaserit) {
         this.laaserit = laaserit;
         this.p1 = p1;
         this.p2 = p2;
     }
-    
+
     /**
      * Metodi muuttaa boolean arvoja perustuen siihen onko nappi painettu.
+     *
      * @param e painettu nappi.
      */
-
     @Override
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
@@ -58,18 +58,26 @@ public class NappaimistonKuuntelija implements KeyListener {
         }
         if (key == KeyEvent.VK_A) {
             p2.getAlus().setKaantyyVasemmalle(true);
-        }        
+        }
     }
-    
+
     /**
-     * metodi muuttaa pelaaiien alusten boolean arvoja
-     * perustuen siihen että nappia ei paineta.
+     * metodi muuttaa pelaaiien alusten boolean arvoja perustuen siihen että
+     * nappia ei paineta.
+     *
      * @param e painamaton nappi.
      */
-
     @Override
     public void keyReleased(KeyEvent e) {
         int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_SPACE) {
+            laaserit.add(p1.getAlus().ammu());
+        }
+
+        if (key == KeyEvent.VK_1) {
+            laaserit.add(p2.getAlus().ammu());
+        }
 
         if (key == KeyEvent.VK_UP) {
             p1.getAlus().setKiihtyyko(false);
@@ -91,23 +99,14 @@ public class NappaimistonKuuntelija implements KeyListener {
             p2.getAlus().setKaantyyVasemmalle(false);
         }
     }
-    
+
     /**
      * tässä pelaajien alukset ampuvat aseilla aina kun nappia on näppäilty.
+     *
      * @param e näppäilty nappi.
      */
-
     @Override
     public void keyTyped(KeyEvent e) {
-        int key = e.getKeyCode();
-        
-        if (key == KeyEvent.VK_SPACE) {
-            laaserit.add(p1.getAlus().ammu());
-        }
-
-        if (key == KeyEvent.VK_TAB) {
-            laaserit.add(p2.getAlus().ammu());
-        }
     }
 
 }
